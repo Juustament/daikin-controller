be9be8f2a0a3a1c121a0515d563ba6e7af6d7764
Juustament <raunoraudsepp@gmail.com>
2026-09-07 13:35:12 +0300

    Update scheduler.ts

--- src/lib/server/scheduler.ts
+++ src/lib/server/scheduler.ts
@@ -631,7 +631,20 @@ export async function executeScheduledTask(
 				planningResult
 			};
 		}
-
+		console.log(
+  'TEMPERATURE_CONTROL_DEBUG',
+  JSON.stringify(
+    devices.map((d: any) => ({
+      id: d.id,
+      managementPoints: d.managementPoints?.map((mp: any) => ({
+        id: mp.id,
+        temperatureControl: mp.temperatureControl
+      }))
+    })),
+    null,
+    2
+  )
+);
 		// Get the first device
 		const device = await getDeviceDetails(accessToken, devices[0].id);
 		const deviceState = parseDeviceState(device);
