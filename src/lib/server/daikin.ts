a1d109afd70d98e690d90d63d0c39b2fc0670cad
Juustament <raunoraudsepp@gmail.com>
2026-09-07 14:34:53 +0300

    Update daikin.ts

--- src/lib/server/daikin.ts
+++ src/lib/server/daikin.ts
@@ -159,12 +159,10 @@ export async function getValidAccessToken(
 /**
  * Make authenticated API request to Daikin
  */
-if (method === 'PATCH') {
-    console.log('DAIKIN_PATCH_DEBUG', JSON.stringify({
-        endpoint,
-        body
-    }));
-}
+console.log('DAIKIN_PATCH_DEBUG', JSON.stringify({
+    endpoint,
+    body
+}));
 async function apiRequest<T>(
 	accessToken: string,
 	endpoint: string,
